7201c6f003e7a55092425bf9dc42826087329302
Adro <acousens@gmail.com>
2021-05-15 11:48:35 -0700

    Something

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <header class="header">
+    <header class="hidden">
       <strong>
         <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
@@ -20,31 +20,3 @@ query {
   }
 }
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,19 +1,47 @@
 <template>
   <Layout>
+    <p>Hi there I'm Adrian.</p>
+    <p>I build web apps...</p>
+    <p>...like this one</p>
+    <section class="relative mb-32">
+      <div class="absolute z-0 opacity-30">
+        <g-image src="~/assets/images/map-dirs.png" />
+      </div>
+      <div class="relative z-10">
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1 class="text-green-500">Hello, world!</h1>
+      <h2 class="text-4xl font-extrabold text-gray-600">Expo layout tool / Indoor mapping</h2>
+      <p>It's a full tradeshow layout builder. It's built with Leaflet.js and runs on Rails.</p>
+      <p>The Expo map is a private, internal app so I can't give you a link</p>
+      <p>But here's a quick walkthrough below <sup><a href="#footnote">*</a></sup> </p>
+      <div class="px-56">
+        <g-image src="~/assets/images/map.png" />
+      </div>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+      <h2 class="text-4xl font-extrabold text-gray-600">Ecosystem</h2>.
+      <div class="px-56">
+        <p>The app is part of this larger system of tools. Data flows in the from the public site (Exhibitors sign up for the show).
+          The manager creates the layout. And then the staff use the map to setup the show.
+        </p>
+        <g-image src="~/assets/images/map-flow.png" />
+      </div>
+      </div>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+      <div class="flex p-4 space-x-4 overflow-hidden" style="max-height: 400px">
+        <div class="w-1/3 overflow-hidden rounded shadow-lg">
+          <g-image src="~/assets/images/map-controller.png"></g-image>
+        </div>
+        <div class="w-1/3 overflow-hidden rounded shadow-lg">
+          <g-image src="~/assets/images/map-haml.png"></g-image>
+        </div>
+        <div class="w-1/3 overflow-hidden rounded shadow-lg">
+          <g-image src="~/assets/images/map-rotater-code.png"></g-image>
+        </div>
+
+      </div>
+      <a id="footnote" class="text-sm">All code / screens shared with permission. All data fakered. </a>
+
+    </section>
 
   </Layout>
 </template>
